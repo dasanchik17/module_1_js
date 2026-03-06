61f52460eff303a4979784309c642ee2d445fa77
Renat <renatdasania8@gmail.com>
2026-03-06 20:52:14 +0300

    feat(list): добавить компонент List с контейнером для донатов

--- src/components/List.js
+++ src/components/List.js
@@ -5,10 +5,19 @@ export class List extends Component {
     this.$rootElement = document.createElement('div');
     this.$rootElement.className = 'donates-container';
 
-    // ...
+    const $titleEl = document.createElement('h2');
+    $titleEl.className = 'donates-container__title';
+    $titleEl.textContent = 'Список донатов';
+
+    this.$rootElement.appendChild($titleEl);
+
+    this.$listContainer = document.createElement('div');
+    this.$listContainer.className = 'donates-container__donates';
+
+    this.$rootElement.appendChild(this.$listContainer);
   }
 
   addItem(item) {
-    // ...
+    this.$listContainer.appendChild(item.$rootElement);
   }
 }
